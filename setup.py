9def9b5463e3f24738c3703b3725637cc5669ab6
Allan Feid <afeid@shutterstock.com>
2014-07-01 11:34:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='netcommander',
-      version='1.1',
+      version='1.2',
       description='A client library to use netconf_proxy',
       author='Allan Feid',
       author_email='allan@ops.guru',
